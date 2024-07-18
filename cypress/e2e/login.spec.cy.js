c19634fbb8bb8000da26f0adbdfc1d73151e1098
João Vitor Condidorio <joaovitorluz010@gmail.com>
2024-07-17 23:23:34 -0300

    Adicionando lista de selectores no projeto

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,26 @@
 describe('testes-site-HRM-orange', () => {
+
+  let selectorList = {
+    usernameField: "[name='username']",
+    passawordField: "[name='password']",
+    loginButton: '.oxd-button',
+    titleTopBar: '.oxd-topbar-header-breadcrumb-module',
+    messageInvalidCredentials: '.oxd-alert',
+  }
+
   it.skip('login-sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passawordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location ('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains ('Dashboard')
+    cy.get(selectorList.titleTopBar).contains ('Dashboard')
   })
   it('login-alerta-falha-usuario-senha', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('usuario_falha_teste')
-    cy.get("[name='password']").type('senha_falha_teste')
-    cy.get("[type='submit']").click()
-    cy.get('.oxd-alert')
+    cy.get(selectorList.usernameField).type('usuario_falha_teste')
+    cy.get(selectorList.passawordField).type('senha_falha_teste')
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.messageInvalidCredentials)
   })
 })
